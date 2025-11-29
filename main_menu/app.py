49240ec3dca7a82596e653d38c86c53d2fcdd21c
Vladimir_R <v_rovda@mail.ru>
2025-11-29 05:06:53 +0300

    Added Lab3

--- main_menu/app.py
+++ main_menu/app.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import Flask, render_template, session, json, redirect,url_for
 from query_bp.route import query_bp
+from reports_bp.reports_routes import report_bp
 from auth.auth_routes import auth_bp
 from access import login_required, group_required
 from flask_assets import Environment, Bundle
@@ -24,6 +25,8 @@ app.secret_key = 'You will never guess'
 app.register_blueprint(query_bp, url_prefix='/query')
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
+
+app.register_blueprint(report_bp, url_prefix='/report')
 
 @app.route('/')
 @login_required
